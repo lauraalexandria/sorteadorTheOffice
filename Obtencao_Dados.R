a562174393c761b62c32afe769843be64cbd8912
lauraalexandria <57160675+lauraalexandria@users.noreply.github.com>
2021-04-11 15:49:12 -0300

    consertando os eps que pareciam estar faltando

--- Obtencao_Dados.R
+++ Obtencao_Dados.R
@@ -9,18 +9,28 @@ geral <- fromJSON(url1)
 poster_geral <- geral$Poster
 num_temp <- as.numeric(geral$totalSeasons)
 
-####### TAREFAS ########
-# 3. Faço o esforço de adicionar os episódios faltantes?
-
 temporada1 <- fromJSON(paste(url2, 1, sep = ''))$Episodes
 temporada2 <- fromJSON(paste(url2, 2, sep = ''))$Episodes
-temporada3 <- fromJSON(paste(url2, 3, sep = ''))$Episodes # Faltam 2
-temporada4 <- fromJSON(paste(url2, 4, sep = ''))$Episodes # Faltam 5
+temporada3 <- fromJSON(paste(url2, 3, sep = ''))$Episodes 
+temporada4 <- fromJSON(paste(url2, 4, sep = ''))$Episodes 
 temporada5 <- fromJSON(paste(url2, 5, sep = ''))$Episodes 
 temporada6 <- fromJSON(paste(url2, 6, sep = ''))$Episodes
-temporada7 <- fromJSON(paste(url2, 7, sep = ''))$Episodes # Faltam 2
+temporada7 <- fromJSON(paste(url2, 7, sep = ''))$Episodes 
 temporada8 <- fromJSON(paste(url2, 8, sep = ''))$Episodes
 temporada9 <- fromJSON(paste(url2, 9, sep = ''))$Episodes # Faltam 2
+
+temporada3[10, 1] <- paste(temporada3[10, 1], "(Parte 1 e 2)")
+temporada3[23, 1] <- paste(temporada3[23, 1], "(Parte 1 e 2)")
+temporada4[1, 1] <- paste(temporada4[1, 1], "(Parte 1 e 2)")
+temporada4[2, 1] <- paste(temporada4[2, 1], "(Parte 1 e 2)")
+temporada4[3, 1] <- paste(temporada4[3, 1], "(Parte 1 e 2)")
+temporada4[4, 1] <- paste(temporada4[4, 1], "(Parte 1 e 2)")
+temporada4[5, 1] <- paste(temporada4[5, 1], "(Parte 1 e 2)")
+temporada4[14, 1] <- paste(temporada4[14, 1], "(Parte 1 e 2)")
+temporada7[11, 1] <- paste(temporada7[11, 1], "(Parte 1 e 2)")
+temporada7[21, 1] <- paste(temporada7[21, 1], "(Parte 1 e 2)")
+temporada9[16, 1] <- paste(temporada9[16, 1], "(Parte 1 e 2)")
+temporada9[21, 1] <- paste(temporada9[21, 1], "(Parte 1 e 2)")
 
 imagens <- data.frame(temp = as.character(1:9),
                       imagem = c("https://i.ibb.co/SxDZC3m/2.png",
