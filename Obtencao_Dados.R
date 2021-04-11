f340adc460e45e4d3846b1c89cc9828cebcb8e2e
lauraalexandria <57160675+lauraalexandria@users.noreply.github.com>
2021-04-11 15:30:05 -0300

    deixando mais bonitinho

--- Obtencao_Dados.R
+++ Obtencao_Dados.R
@@ -10,12 +10,7 @@ poster_geral <- geral$Poster
 num_temp <- as.numeric(geral$totalSeasons)
 
 ####### TAREFAS ########
-# 2. Qual o pacote que mostra imagens mesmo?
 # 3. Faço o esforço de adicionar os episódios faltantes?
-# 4. Pensar em Inputs de letras bonitas
-# 5. Coloco alguma música?
-# 6. Não esquecer da logo do DunderMifflin
-# 7. Colocar uma imagem inicial antes
 
 temporada1 <- fromJSON(paste(url2, 1, sep = ''))$Episodes
 temporada2 <- fromJSON(paste(url2, 2, sep = ''))$Episodes
@@ -28,15 +23,15 @@ temporada8 <- fromJSON(paste(url2, 8, sep = ''))$Episodes
 temporada9 <- fromJSON(paste(url2, 9, sep = ''))$Episodes # Faltam 2
 
 imagens <- data.frame(temp = as.character(1:9),
-                      imagem = c("https://images-na.ssl-images-amazon.com/images/S/pv-target-images/278528df7eb08b4e1d09281cf8fddec21f9a58288a232423e4d65c1258a3ca1a._RI_V_TTW_.jpg",
-                                 "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/1258b0e14ccd79bfe1624be6355a29f90f490a92e52060e9e977921053fce7ea._RI_V_TTW_.jpg",
-                                 "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/2d358f2d255637d31ca7880cb0573e11c1aefa6a146221139ce1d02407304c61._RI_V_TTW_.jpg",
-                                 "http://br.web.img2.acsta.net/pictures/20/08/16/19/11/0428207.jpg",
-                                 "https://images-na.ssl-images-amazon.com/images/I/51Slake1xDL._AC_SY445_.jpg",
-                                 "https://static.wikia.nocookie.net/theoffice/images/b/b4/250px-TheOffice_S6_DVD.jpg/revision/latest/top-crop/width/360/height/360?cb=20200714002248",
-                                 "https://images-na.ssl-images-amazon.com/images/I/51V0BCZiznL._AC_SY445_.jpg",
-                                 "http://br.web.img3.acsta.net/pictures/20/08/16/19/55/5474261.jpg",
-                                 "https://static.wikia.nocookie.net/theoffice/images/9/9c/The_Office_Season_9.jpg/revision/latest/top-crop/width/360/height/360?cb=20200610120310"))
+                      imagem = c("https://i.ibb.co/SxDZC3m/2.png",
+                                 "https://i.ibb.co/YRvG5Rw/3.png",
+                                 "https://i.ibb.co/QDDPFmC/4.png",
+                                 "https://i.ibb.co/KKHQ2Hx/5.png",
+                                 "https://i.ibb.co/tH5HLzd/6.png",
+                                 "https://i.ibb.co/4m0gPxx/7.png",
+                                 "https://i.ibb.co/WG4qjBd/8.png",
+                                 "https://i.ibb.co/kcmst7Q/9.png",
+                                 "https://i.ibb.co/XJLvXjM/10.png"))
 
 
 geral <- rbind(temporada1 %>% mutate(temp = "1"), 
